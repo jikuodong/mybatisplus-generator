059a2ae1468678ec0b122ff31271917d326a2a6d
姬阔东 <jikuodong@froeasy.net>
2019-09-17 15:55:05 +0800

    提交mybatis plus 自动生成代码

---
+++ generator/src/test/java/net/froeasy/wpc/wpc/WpcApplicationTests.java
@@ -0,0 +1,16 @@
+package net.froeasy.wpc.wpc;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class WpcApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
